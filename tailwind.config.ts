c93ac4b8364663c357aadc984d29b5fc15e65ad8
Tu Nombre <hola@solonet.es>
2025-12-03 18:22:08 +0100

    intentando despliegue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
     content: [
-        "./app/**/*.{js,ts,jsx,tsx,mdx}", // Only scan the app directory
+        "./app/**/*.{js,ts,jsx,tsx,mdx}", // Only scan the apps directory
         "./components/**/*.{js,ts,jsx,tsx,mdx}", // And the components directory
     ],
     darkMode: "class",
